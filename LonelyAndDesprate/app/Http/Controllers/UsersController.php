c67a78c8b1c111994602247a2dc9eb31a4aee4b7
YehyaShahbur <mohamad.yehya.shahbur@gmail.com>
2021-10-03 17:24:18 +0300

    added comments & README file

--- LonelyAndDesprate/app/Http/Controllers/UsersController.php
+++ LonelyAndDesprate/app/Http/Controllers/UsersController.php
@@ -20,15 +20,18 @@ class UsersController extends Controller
 {
     public function getUsers(){
 
-        $userGender = auth()->user()->gender;
-        $userId = auth()->user()->id;
+        $userGender = auth()->user()->gender; // get user gender
+        $userId = auth()->user()->id; // get user id
 
-        $favoriteRequestId = FavoriteRequest::where('favorite_requests.from', '=', $userId)->pluck('to')->all();
+        $favoriteRequestId = FavoriteRequest::where('favorite_requests.from', '=', $userId)
+        ->pluck('to')->all();  // getting ids of people who favorite user
 
+        // get people who are matched with user
         $matchedForUserId_1 = MatchUser::where('matches.user_1', '=', $userId)->pluck('user_2')->all();
         $matchedForUserId_2 = MatchUser::where('matches.user_2', '=', $userId)->pluck('user_1')->all();
         $matchedUsers = array_merge($matchedForUserId_1,$matchedForUserId_2);
 
+        // selecting users who are not matched or already been favorited with opposite gender & not admin 
         $users = User::whereNotIn('id', $favoriteRequestId)
         ->whereNotIn('id', $matchedUsers)
         ->select("*")
@@ -42,6 +45,7 @@ class UsersController extends Controller
 
     public function updateUser(Request $request){
 
+        // validate input
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
             'age' => 'required|string|between:2,100',
@@ -55,6 +59,7 @@ class UsersController extends Controller
 
 
         $userId = auth()->user()->id;
+        // update user information
         $user = DB::table('users')
               ->where('id', $userId)
               ->update(['name' => $request -> name,
@@ -71,15 +76,18 @@ class UsersController extends Controller
 
     public function search(Request $request){
 
-        $userGender = auth()->user()->gender;
-        $userId = auth()->user()->id;
+        $userGender = auth()->user()->gender; // get user gender
+        $userId = auth()->user()->id; // get user id
 
-        $favoriteRequestId = FavoriteRequest::where('favorite_requests.from', '=', $userId)->pluck('to')->all();
+        $favoriteRequestId = FavoriteRequest::where('favorite_requests.from', '=', $userId)
+        ->pluck('to')->all(); // getting ids of people who favorite user
 
+        // get people who are matched with user
         $matchedForUserId_1 = MatchUser::where('matches.user_1', '=', $userId)->pluck('user_2')->all();
         $matchedForUserId_2 = MatchUser::where('matches.user_2', '=', $userId)->pluck('user_1')->all();
         $matchedUsers = array_merge($matchedForUserId_1,$matchedForUserId_2);
 
+        // selecting users who are not matched or already been favorited with opposite gender & not admin based on search-input 
         $users = User::whereNotIn('id', $favoriteRequestId)
         ->whereNotIn('id', $matchedUsers)
         ->select("*")
@@ -95,18 +103,22 @@ class UsersController extends Controller
         // check uf userid in to  -> if yes (remove the record where the userid is in to) & add record to matches table & add to notification table from userid to requestid
         // if no add to notification table from userid to requestid
 
-        $userId = auth()->user()->id;
+        $userId = auth()->user()->id; 
         $userName = auth()->user()->name;
 
+        // get every instance where user has been favorited before
         $favoriteRequestId = FavoriteRequest::where('favorite_requests.to', '=', $userId)->where('favorite_requests.from', '=', $request -> id)->pluck('from')->all();
 
+        // id not favorited before...
         if(empty($favoriteRequestId)){
 
+            // add record to favorite table
             $fav = new FavoriteRequest;
             $fav -> from = $userId;
             $fav -> to = $request -> id;
             $fav -> save();
 
+            // send notification to the favorited user
             $notif = new Notification;
             $notif -> from = $userId;
             $notif -> to = $request -> id;
@@ -116,13 +128,17 @@ class UsersController extends Controller
         return response()->json(['message' => 'request sent successfully'], 200); 
 
         }else{
+
+            // delete from favorite table already existing record
             FavoriteRequest::where('favorite_requests.to', '=', $userId)->where('favorite_requests.from', '=', $request -> id)->delete();
 
+            // add both users to matches table
             $match = new MatchUser;
             $match -> user_1 = $userId;
             $match -> user_2 = $request -> id;
             $match -> save();
 
+            // send notification to the matched user
             $notif = new Notification;
             $notif -> from = $userId;
             $notif -> to = $request -> id;
@@ -134,6 +150,8 @@ class UsersController extends Controller
     }
 
     public function addMessage(Request $request){
+
+        // add record to messages, waiting for admin to accept
         $userId = auth()->user()->id;
         $msg = new Chat;
         $msg -> from = $userId;
@@ -147,6 +165,8 @@ class UsersController extends Controller
     }
 
     public function getMatched(){
+
+        // get all matched users
         $userId = auth()->user()->id;
         $users = DB::table('matches')
         ->where('user_1', '=', $userId)
@@ -157,6 +177,8 @@ class UsersController extends Controller
     }
 
     public function getUserPics(){
+
+        // get all user pics
         $userId = auth()->user()->id;
         $pics = DB::table('pictures')
         ->where('user_id', '=', $userId)
---
+++ README.txt
@@ -0,0 +1,31 @@
+users APIs:
+
+usersController
+
+1- api to display all women users if user is a man and vise versa  //DONE
+2- api to add record to pictures table with pending 0 (waiting for admin approvale)  // 1/2 DONE
+3- api to edit profile  // DONE
+4- api to search and gender // DONE
+5- api to add record to favorite table & check if the user is in both column from and to 
+=> if yes (delete record and add to match table) + add record to notification table
+=> if no add record + notify target user(to)  // DONE
+6- api to send messages between matched users (from..to..message..) + pending set to 0 // DONE
+7- api to display user infos and pics //DONE
+8- api to get matched users //DONE
+
+admin APIs:
+
+adminsController
+
+1- api to display all infos from pics table
+2- api to approve uploading pic => change pending to 1 for specific pic id 
+3- api to decline pic => delete record for specific pic id
+4- api to approve uploading msgs => change pending to 1 for specific msg id + add record to notification table
+5- api to decline msg => delete record for specific msg id
+
+
+----------------------------------------
+
+IMPORTANT FOR JWT:
+
+composer require tymon/jwt-auth --ignore-platform-reqs
